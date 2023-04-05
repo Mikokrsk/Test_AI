a510c5af3bc726f49b3ec65d6a851ec7b14d458d
Mykola Krasko <mikokrsk@gmail.com>
2023-04-05 13:21:36 +0300

    time (1:32:00) +class Lauer +class Topology +class NeuralNetwork

---
+++ Test_AI/Layer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_AI
+{
+    public class Layer
+    {
+        public List<Neuron> Neurons;
+        //перевірка чи к-сть не null
+        public int Count => Neurons?.Count ?? 0;
+
+        public Layer(List<Neuron> neurons, NeuronType type = NeuronType.Normal) 
+        {
+            Neurons= neurons;
+        }
+
+    }
+}
---
+++ Test_AI/Topology.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_AI
+{
+    public class Topology
+    {
+        //к-сть вхідних даних в нейро мережу
+        public int InputCount { get; }
+        //к-сть виходів
+        public int OutputCount { get; }
+        //к-сть нейронів в схованих шарах
+        public List<int> HiddenLayers { get; }
+        
+        public Topology(int inputCount,int outputCount,params int[] layers)
+        {
+            InputCount = inputCount;
+            OutputCount = outputCount;
+            HiddenLayers = new List<int>();
+            HiddenLayers.AddRange(layers);
+        }
+    }
+}
