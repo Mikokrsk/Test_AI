431c80ace41e35459366167f1f3ac6305518f3ae
Mykola Krasko <mikokrsk@gmail.com>
2023-04-05 12:21:58 +0300

    Добавьте файлы проекта.

---
+++ Test_AI/NeuronType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_AI
+{
+    //тип нейрона
+   public enum NeuronType
+    {
+        Input = 0,
+        Normal = 1,
+        Output = 2
+    }
+}
